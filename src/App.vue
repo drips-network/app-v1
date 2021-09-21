a885b604d6d88d24c6d3cd211757d1b2befd4074
evvvritt <evv.info@gmail.com>
2021-09-21 17:11:00 +0200

    init, tailwind, pug, header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import SvgLogo from './components/SvgLogo.vue'
+</script>
+
+<template lang="pug">
+.max-w-screen-2xl.mx-auto.p-3
+  header.h-20.rounded-full.bg-indigo-700.flex.items-center.justify-between
+    div.pl-9
+      svg-logo.h-5.text-white
+  main#main
+    router-view
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1440px'
+      },
+      colors: {
+        indigo: {
+          700: '#17164B',
+          900: '#090928'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
