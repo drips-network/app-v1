b20216c16b95bf1347d5c2eae9afac7d4ec57489
evvvritt <evv.info@gmail.com>
2021-10-04 14:40:41 +0200

    add styles, svglogo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import SvgLogo from './components/SvgLogo.vue'
+</script>
+
+<template lang="pug">
+.max-w-screen-2xl.mx-auto.p-3
+  header.h-20.rounded-full.bg-indigo-700.flex.items-center.justify-between
+    div.pl-9
+      svg-logo.h-5.text-white
+  //- main#main
+    router-view
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
