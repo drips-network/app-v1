0553cc49cae72b15ada220f9db3c59650ddb4a5b
evvvritt <evv.info@gmail.com>
2022-01-17 23:56:28 +0100

    clean up headerLarge, del DripsAll

--- src/components/HeaderLarge.vue
+++ src/components/HeaderLarge.vue
@@ -3,15 +3,9 @@ const props = defineProps(['icon'])
 </script>
 
 <template lang="pug">
-.w-full.mb-96.flex.justify-center.stickyff.z-20.top-6
+.w-full.flex.justify-center
   .flex.bg-indigo-950.borderff.border-violet-700.rounded-full.items-center
     .h-44.w-44.flex.items-center.justify-center.text-base {{ props.icon }}
     .-ml-6.text-base.font-semibold.pr-20.text-violet-650
       slot
-//- small
-  .w-full.flex.justify-center.mb-64
-    .flex.bg-indigo-950.border.border-violet-700.rounded-full.items-center
-      .h-44.w-44.flex.items-center.justify-center.text-md {{ props.icon }}
-      .-ml-6.text-base.font-semibold.pr-20.text-violet-650
-        slot
 </template>
--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,16 +1,17 @@
 <script setup>
-import { ref, onBeforeMount } from 'vue'
+import { ref, computed, onBeforeMount } from 'vue'
 import api from '@/api'
 import ProjectThumb from '@/components/ProjectThumb'
 import LoadingBar from '@/components/LoadingBar'
 import DripRow from '@/components/DripRow'
 import Addr from '@/components/Addr'
 import UserAvatar from '@/components/UserAvatar'
-import DripsAll from '@/components/DripsAll'
+// import DripsAll from '@/components/DripsAll'
 import InfoBar from '@/components/InfoBar'
 import HeaderLarge from '@/components/HeaderLarge'
 import SpotlightRecipient from '@/components/SpotlightRecipient'
 import store from '@/store'
+import { formatSplitsEvents } from '@/utils'
 
 const spotlights = [
   // soliditylang.eth
@@ -51,6 +52,7 @@ const getProjects = async () => {
 }
 
 const splits = ref()
+const dripRows = computed(() => splits.value && formatSplitsEvents(splits.value))
 const getSplits = async () => {
   try {
     const events = await store.dispatch('getSplitsReceivers')
@@ -78,12 +80,8 @@ onBeforeMount(() => {
 
 <template lang="pug">
 article.explore.pt-56.px-24
-  //- header.flex.mt-12
-    h1.h-80.rounded-full.flex.border.border-violet-600.ffbg-indigo-700.text-2xl.font-semibold.items-center.px-28.text-violet-650 Explore
-  //- featured
   section
-    //- h2.sr-only Highlights
-    header-large(icon="✨")
+    header-large.mb-96(icon="✨")
       header
         h2 Spotlight
 
@@ -93,44 +91,35 @@ article.explore.pt-56.px-24
 
   //- communities
   section.mt-240
-    //- header-large.mb-96(icon="🙂")
-      h2 Communities
-    //- .flex.justify-center
-      .h-80.bg-indigo-950.borderff.border-violet-800.pr-40.text-xl.rounded-full.flex.items-center.text-violet-650.font-semibold
-        .ml-32.mr-16 🙂
-        | Communities
+    //- (loading)
+    template(v-if="!projects")
+      loading-bar
 
-    section.mt-56
-      template(v-if="!projects")
-        loading-bar
+    //- (list)
+    template(v-else)
+      header-large.mb-56(icon="🙂")
+        header
+          h2 #[b Communities] raising funds with #[b NFT memberships] 🧩
 
-      template(v-else)
-        section.px-12ff
-          .w-full.mb-56.flex.justify-center.stickyff.z-20.top-6
-            .flex.bg-indigo-950.borderff.border-violet-700.rounded-full.items-center
-              .h-44.w-44.flex.items-center.justify-center.text-base 🙂
-              .-ml-6.text-base.font-semibolffd.pr-20.text-violet-650
-                | #[b Communities] raising funds with #[b NFT memberships] 🧩
-          //- info-bar.mb-16.text-md
-            .w-full.text-center.px-32
-              | These #[b communities] are raising funds with #[b NFT memberships] 🧩
-
-          //- .my-40.text-base.text-violet-650.text-center
-            | #[b {{ projects.length }} communities] are #[b raising funds] with #[b NFT memberships] 🧩
-
-          ul
-            //- projects...
-            li(v-for="project in projects")
-              project-thumb.mb-32(:project="project")
-
-          //- footer.mt-56.flex.justify-center
-            router-link.btn.btn-xl.btn-dark.px-60(to="/create") Create ✨
+      ul
+        //- projects...
+        li(v-for="project in projects")
+          project-thumb.mb-32(:project="project")
 
   //- drips
   section.mt-220
-    //- header-large.mb-96(icon="💧")
-      h2 Drips
+    //- (loading)
+    template(v-if="!splits")
+      loading-bar
 
-    section.mt-48.pb-40
-      drips-all.text-lg(:splits="splits")
+    //- (list)
+    template(v-else)
+      header-large.mb-56(icon="💧")
+        header
+          h2 #[b {{ dripRows.length }} address] are #[b dripping] to others.
+
+      section
+        ul
+          li(v-for="drip in dripRows")
+            drip-row.my-4(:drip="drip")
 </template>
