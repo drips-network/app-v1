c9823a18c5b4ee6f5cba3a5a938da76fae06cc34
evvvritt <evv.info@gmail.com>
2022-02-06 21:54:32 +0100

    hide polygon warning for now

--- src/components/WarningPolygonAddresses.vue
+++ src/components/WarningPolygonAddresses.vue
@@ -3,5 +3,5 @@ import FormMessage from '@/components/FormMessage'
 </script>
 
 <template lang="pug">
-form-message(:body="{status: -2, message: 'Any funds sent to Ethereum contracts or multi-sig addresses on Polygon are <b>permanently lost!</b> Please ensure you are only sending funds to <b>individual wallet addresses</b>.'}")
+//- form-message(:body="{status: -2, message: 'Any funds sent to Ethereum contracts or multi-sig addresses on Polygon are <b>permanently lost!</b> Please ensure you are only sending funds to <b>individual wallet addresses</b>.'}")
 </template>
