8683e990528e4fc6fa3f43fbee0b0c1e80aff9d4
evvvritt <evv.info@gmail.com>
2022-05-18 17:54:33 +0200

    use etherscan blockies - fixes #80

--- src/components/AvatarBlockie.vue
+++ src/components/AvatarBlockie.vue
@@ -1,46 +1,30 @@
 <script setup>
-import { ref, onMounted, onUpdated } from 'vue'
-import blockies from 'ethereum-blockies'
-
-const el = ref(null)
+import { ref, onMounted } from 'vue'
+import { createIcon } from '@/plugins/blockies.ts'
 
 const props = defineProps({
   address: String,
-  width: { type: String, default: '72' }
+  // width: { type: String, default: '72' }
 })
 
-const columns = 8
-const scale = props.width * (window.devicePixelRatio || 1) / columns
-let icon
+const imgUrl = ref()
 
-function generate () {
-  icon = blockies.create({ // All options are optional
-    seed: props.address.toLowerCase(), // seed used to generate icon data, default: random
-    // color: '#dfe', // to manually specify the icon color, default: random
-    // bgcolor: '#aaa', // choose a different background color, default: random
-    size: columns, // width/height of the icon in blocks, default: 8
-    scale: scale // width/height of each block in pixels, default: 4
-    // spotcolor: '#000' // each pixel has a 13% chance of being of a third color,
-    // default: random. Set to -1 to disable it. These "spots" create structures
-    // that look like eyes, mouths and noses.
-  })
+const generate = () => {
+  const avatar = createIcon({
+    seed: props.address.toLowerCase(),
+    size: 8,
+    scale: 16,
+  });
+
+  imgUrl.value = avatar.toDataURL()
 }
 
-// twice because 1x is black ???? (https://github.com/ethereum/blockies/pull/6)
-generate()
-generate()
-
-onMounted(() => {
-  el.value.appendChild(icon)
-})
+onMounted(() => generate())
 </script>
 
 <template lang="pug">
-.avatar-blockie.rounded-full.overflow-hidden.relative(ref="el")
+img.avatar-blockie.rounded-full.overflow-hidden(:src="imgUrl", :alt="`Blockie avatar of ${props.address}`", :title="props.address")
 </template>
 
 <style>
-.avatar-blockie canvas{
-  width: 100%;
-}
 </style>
