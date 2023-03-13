1e9dad5fedebefbd120194695449e11a04d62d62
evvvritt <evv.info@gmail.com>
2023-03-13 15:42:32 +0100

    version changer next to logo, v2 links to beta.drips.network

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@ const profileBannerVisible = computed(() => {
   return !hasConnected.value && !store.state.address && !route.name?.startsWith('create') && route.meta.type !== 'legal'
 })
 const testnetsVisible = ref(false)
+const versionMenuVisible = ref(false)
 
 store.dispatch('init')
 
@@ -85,14 +86,32 @@ const switchToAppNetwork = async () => {
     header.relative.z-30.flex.items-center.justify-center.md_justify-between.px-12
       //- left side (mobile centered)
       .flex.w-full.justify-between.md_justify-start
-        router-link.h-80.rounded-full.bg-indigo-700.flex.items-center.pr-36.btn-focus-violet(to="/")
+        .h-80.rounded-full.bg-indigo-700.flex.items-center.pr-24
           //- home / landing site link
-          .flex.items-center.-mt-px
+          router-link.flex.items-center.-mt-px.text-violet-650.notouch_hover_text-white.transition.duration-150(to="/")
+            //- drip logo
             .w-80.flex.justify-center.items-center.pb-px.-mr-6
               svg-logo-drop(style="height:3.6rem")
-            svg-logo.text-violet-650(style="height:3.2rem")
+            //- title logo
+            svg-logo(style="height:3.2rem")
 
-        //- network changer
+          //- version changer
+          .relative.z-50.ml-20.leading-none.font-semibold.text-greenbright-400.text-ms
+            //- network menu btn
+            button.relative.z-10.rounded-lg.pl-20.py-12.flex.items-center(@click="versionMenuVisible = !versionMenuVisible", :class="{'bg-indigo-950': !versionMenuVisible, 'notouch_hover_ring notouch_hover_ring-violet-650': !versionMenuVisible }")
+              .transform.-translate-y-1 v1
+              //- chevron icon
+              .w-6.h-6.ml-10.mr-16.border-r.border-b.border-current.opacity-50.transform.origin-center(:class="{'-rotate-135': networkMenu, 'rotate-45 -translate-y-2': !networkMenu}")
+
+            //- (network menu dropdown)
+            ul.absolute.min-w-full.top-0.left-0.py-12.bg-indigo-950.rounded-md(v-show="versionMenuVisible")
+              li.py-2 &nbsp;
+              //- ethereum link
+              li
+                a.inline-block.py-5.px-20.transition.duration-150.notouch_hover_text-white(href="https://beta.drips.network", class="text-greenbright-400/90")
+                  | v2
+
+        //- network switcher
         .ml-16.h-80.rounded-full.bg-indigo-900.flex.items-center.text-ms.leading-none.font-semibold
           .relative.z-50
             //- network menu btn
@@ -106,20 +125,20 @@ const switchToAppNetwork = async () => {
               li.py-2 &nbsp;
               //- ethereum link
               li(v-if="networkName !== 'mainnet'")
-                a.inline-block.py-5.px-20.transition.duration-150.notouch_hover_text-white(href="https://app.drips.network", class="text-greenbright-400/90")
+                a.inline-block.py-5.px-20.transition.duration-150.notouch_hover_text-white(href="https://app.v1.drips.network", class="text-greenbright-400/90")
                   template(v-if="networkName.includes('polygon')") ethereum
                   template(v-else) mainnet
               //- rinkeby link
-              li(v-if="testnetsVisible && networkName !== 'rinkeby'")
-                a.inline-block.py-5.px-20.transition.duration-150.opacity-75.notouch_hover_opacity-100.notouch_hover_text-white(href="https://rinkeby.drips.network", class="text-greenbright-400/90")
+              //- li(v-if="testnetsVisible && networkName !== 'rinkeby'")
+                a.inline-block.py-5.px-20.transition.duration-150.opacity-75.notouch_hover_opacity-100.notouch_hover_text-white(href="https://rinkeby.v1.drips.network", class="text-greenbright-400/90")
                   | rinkeby
               //- polygon link
               li(v-if="networkName !== 'polygon'")
-                a.inline-block.py-5.px-20.text-violet-650.transition.duration-150.notouch_hover_text-white(href="https://polygon.drips.network")
+                a.inline-block.py-5.px-20.text-violet-650.transition.duration-150.notouch_hover_text-white(href="https://polygon.v1.drips.network")
                   | polygon
               //- mumbai link
               li(v-if="testnetsVisible && networkName !== 'mumbai'")
-                a.inline-block.py-5.px-20.text-violet-650.transition.duration-150.opacity-75.notouch_hover_opacity-100.notouch_hover_text-white(href="https://mumbai.drips.network")
+                a.inline-block.py-5.px-20.text-violet-650.transition.duration-150.opacity-75.notouch_hover_opacity-100.notouch_hover_text-white(href="https://mumbai.v1.drips.network")
                   | mumbai
               //- (testnets button)
               li(v-show="!testnetsVisible")
